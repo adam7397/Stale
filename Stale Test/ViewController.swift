929dd35308012650427ef58178438f365aa8ed1e
Adam Bradfield <adambradfield@adams-school-mac.local>
2018-06-11 20:03:39 -0400

    Initial Commit

---
+++ Stale Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stale Test
+//
+//  Created by Adam Bradfield on 6/11/18.
+//  Copyright © 2018 Adam Bradfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
