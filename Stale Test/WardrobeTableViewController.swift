fec49109dec1b0abb4665174a50cca1fac5f025f
Adam Bradfield <adambradfield@adams-school-mac.local>
2018-06-29 09:28:10 -0400

    This time we got the table view popping for an array. need to convert sql to csv or csv to array idk

---
+++ Stale Test/WardrobeTableViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WardrobeTableViewController.swift
+//  Stale Test
+//
+//  Created by Adam Bradfield on 6/27/18.
+//  Copyright © 2018 Adam Bradfield. All rights reserved.
+//
+
+import UIKit
+
+class WardrobeTableViewController: UITableViewController {
+
+    var clothes = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+                   "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+                   "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+                   "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+                   "Pear", "Pineapple", "Raspberry", "Strawberry"]
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return clothes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        let clothesname = clothes[indexPath.row]
+        cell.textLabel?.text = clothes[indexPath.row]
+        cell.detailTextLabel?.text = "Very cool!"
+    //    cell.imageView?.image = UIImage(named: clothesname)
+        
+        return cell
+    }
+ 
+}
